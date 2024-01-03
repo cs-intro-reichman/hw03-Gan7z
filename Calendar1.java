2800d5ec290619739ff0952cf31637eaa75f3f2e
Gan7z <oshri.gantz@post.runi.ac.il>
2024-01-03 09:15:14 +0200

    Beta version 0.0.1 - Calendar1
    Unfinished version of Calendar1 , WIP

--- Calendar1.java
+++ Calendar1.java
@@ -42,7 +42,8 @@ public class Calendar1 {
 		 
     // Returns true if the given year is a leap year, false otherwise.
 	private static boolean isLeapYear(int year) {
-	    // Replace the following statement with your code
+	     if (year % 4 == 0)
+			return true;
 		return false;
 	}
 	 
@@ -51,7 +52,32 @@ public class Calendar1 {
 	// February has 28 days in a common year, and 29 days in a leap year.
 	// All the other months have 31 days.
 	private static int nDaysInMonth(int month, int year) {
-		// Replace the following statement with your code
-		return 0;
+		switch (month){
+			case 1:
+				return 31;
+			case 2:
+				return  isLeapYear(year) ? 29 : 28; 
+			case 3:
+				return 31;
+			case 4:
+				return 30;
+			case 5:
+				return 31;
+			case 6:
+				return 30;
+			case 7:
+				return 31;
+			case 8:
+				return 31;
+			case 9:
+				return 30;
+			case 10:
+				return 31;
+			case 11:
+				return 30;
+			case 12:
+				return 31;
+			default:
+				return 0;
 	}
 }
